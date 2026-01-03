61b03004bc7b8a0eed3f87f9877ee8c94e4ee6a2
nounrr <sosi66581@gmail.com>
2026-01-03 16:38:42 +0100

    Update default port configuration to 3600 in ecosystem settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         HOST: process.env.HOST || '0.0.0.0',
-        PORT: process.env.PORT || '3400',
+        PORT: process.env.PORT || '3600',
         WA_API_KEY: process.env.WA_API_KEY || 'Q9Kx6Q2f5pQAU1uYIY0YyWJp2Zb7e1w60H8rFf3yERZ3',
         DEFAULT_CC: process.env.DEFAULT_CC || '212',
         CHROME_PATH: process.env.CHROME_PATH || '/usr/bin/google-chrome',
